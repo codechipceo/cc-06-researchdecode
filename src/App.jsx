924675c03793334cda4cec7a7173975fe2726a15
Mansab Mir <mansabmirsde@gmail.com>
2024-06-08 13:52:42 +0530

    Landing page

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { SearchPapers } from "./Pages/SearchPapers/SearchPapers";
 import SignIn from "./Pages/SignIn/SignIn";
 import SignUp from "./Pages/SingUp/SignUp";
+import ResponsiveAppBar from "./Components/Navbar/Navbar";
 
 export default function App() {
   return (
+    <>
+      <ResponsiveAppBar />
     <BrowserRouter>
       <Routes>
         <Route path='/signup' element={<SignUp />} />
@@ -12,5 +15,6 @@ export default function App() {
         <Route path='/search-paper' element={<SearchPapers />} />
       </Routes>
     </BrowserRouter>
+    </>
   );
 }
