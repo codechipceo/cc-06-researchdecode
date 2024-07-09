6c064c415ed90bc5ce39da904f0ee9fbd5e42786
Mansab Mir <mansabmirsde@gmail.com>
2024-07-09 12:49:20 +0530

    Request link updated

--- src/Components/requestLink.jsx
+++ src/Components/requestLink.jsx
@@ -10,36 +10,37 @@ import { useNavigate } from "react-router-dom";
 
 const RequestLink = ({ request }) => {
   const navigate = useNavigate();
-
+  console.log(request);
+  const { DOI_number, paperDetail, requestBy } = request;
+  const { title, author } = paperDetail;
   const handleSend = () => {
-    navigate(`/inbox/${request.userId}`);
+    navigate(`/inbox/${request.requestBy}`);
   };
 
   return (
     <ListItem alignItems='flex-start' sx={{ borderBottom: "1px solid #ddd" }}>
       <ListItemText>
         <Typography variant='h6' component='div'>
-          {request.title[0]}
+          {title[0]}
         </Typography>
-        <Typography variant='subtitle1' component='div'>
-          Author: {request.author}
-        </Typography>
+        {/* <Typography variant='subtitle1' component='div'>
+          Author: {author[0]}
+        </Typography> */}
         <Typography variant='body2' component='div'>
           DOI:{" "}
           <a
-            href={`https://doi.org/${request.doi}`}
             target='_blank'
             rel='noopener noreferrer'
           >
-            {request.doi}
+            {DOI_number}
           </a>
         </Typography>
-        <Typography variant='body2' component='div'>
+        {/* <Typography variant='body2' component='div'>
           PDF Link:{" "}
-          <a href={request.link} target='_blank' rel='noopener noreferrer'>
+          <a href={} target='_blank' rel='noopener noreferrer'>
             View PDF
           </a>
-        </Typography>
+        </Typography> */}
       </ListItemText>
       <ListItemSecondaryAction>
         <IconButton edge='end' aria-label='send' onClick={handleSend}>
