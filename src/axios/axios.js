56dad90da5a79f839258efbb19622ad241ea8fad
Mansab Mir <mansabmirsde@gmail.com>
2024-07-31 23:57:24 +0530

    envrioment configuration done

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
+import { activeBaseUrl } from "./config";
 
-const devUrl = "http://localhost:5000/";
 export const axiosInstance = axios.create({
-  baseURL: devUrl,
+  baseURL: activeBaseUrl,
 });
 
 axiosInstance.interceptors.request.use(
