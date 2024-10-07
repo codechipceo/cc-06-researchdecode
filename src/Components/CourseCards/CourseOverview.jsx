6573db15457bd320504f3af2a977233974627348
Mansab Mir <mansabmirsde@gmail.com>
2024-10-07 15:39:30 +0530

    course overview updated

--- src/Components/CourseCards/CourseOverview.jsx
+++ src/Components/CourseCards/CourseOverview.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
+
 
 const CourseOverview = ({ course }) => {
   return (
@@ -9,11 +8,7 @@ const CourseOverview = ({ course }) => {
       <Typography variant='h6' paragraph>
         Course Overview
       </Typography>
-      <ReactQuill
-        value={course.courseDescription} // Pass the course description as value
-        readOnly={true} // Make it read-only so it's non-editable
-        theme='bubble'
-      />
+     
     </Box>
   );
 };
