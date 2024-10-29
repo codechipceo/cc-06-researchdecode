75f1e980854a1f276ecbaee35963d8110c7cfeeb
Samiran_Das <samiran4209@gmail.com>
2024-10-29 23:13:26 +0530

    cc-86cwx6m7z-FAQ-Card-Componen resolve

--- src/Components/FAQCardComponent/FaqCardComponent.jsx
+++ src/Components/FAQCardComponent/FaqCardComponent.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import "./FaqCardComponent.scss";
 import Typography from "../../assets/scss/components/Typography";
+import { Panel } from 'rsuite';
 
 const FaqCardComponent = ({ header, content }) => {
   return (
-    <div className="faq-card">
+    <Panel className="faq-card">
       <Typography size="lg" variant="bold" className="faq-card__header">
         {header}
       </Typography>
       <Typography size="md" variant="regular" className="faq-card__content">
         {content}
       </Typography>
-    </div>
+    </Panel>
   );
 };
 
