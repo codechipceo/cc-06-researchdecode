5494e32dae1a2b78cf8e799d94394b5144e860ee
Mansab Mir <mansabmirsde@gmail.com>
2024-11-10 20:24:57 +0530

    Collab page

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import VideoCall from "./Pages/WebRTC/WebRTC";
+import Collaboration from "./Pages/Collaboation.jsx/Collaboration";
 
 const router = createBrowserRouter([
   {
@@ -89,6 +90,10 @@ const router = createBrowserRouter([
   {
     path: "/videocall/:peerId",
     element:<VideoCall />
+  }
+,  {
+    path: "/collaboration",
+    element:<Collaboration />
   }
 ]);
 
--- src/Components/Headers/HeaderThree.jsx
+++ src/Components/Headers/HeaderThree.jsx
@@ -3,41 +3,38 @@ import ResponsiveAppBar from "../Navbar/Navbar";
 import { useNavigate } from "react-router-dom";
 import Typography from "../../assets/scss/components/Typography"; // Import your custom typography component
 import "./HeaderThree.scss"; // Import SCSS for custom styles
-
+import defaultBgImage  from '../../assets/Images/bgrp.png'
 export const HeaderThree = ({ title, breadcrumbPath ,backgroundImage}) => {
 
   const navigate = useNavigate();
 
   return (
-   
-    <div className="header-three">
+    <div className='header-three'>
       <ResponsiveAppBar />
       <div
-        className="banner"
-        style={{ backgroundImage: `url(${backgroundImage})` }}
+        className='banner'
+        style={{ backgroundImage: `url(${backgroundImage ?? defaultBgImage})` }}
       >
-        <Typography size="5xl" variant={"semibold"} className="title">
+        <Typography size='5xl' variant={"semibold"} className='title'>
           {title}
         </Typography>
       </div>
-      <div className="breadcrumbs" aria-label="breadcrumb">
+      <div className='breadcrumbs' aria-label='breadcrumb'>
         {breadcrumbPath.map((crumb, index) => (
-           <React.Fragment key={index}>
-          <button
-            key={index}
-            className="breadcrumb-button"
-            onClick={() => navigate(crumb.path)}
-          >
-            {crumb.label}
-          </button>
-           <span className="breadcrumb-separator">{">"}</span> 
+          <React.Fragment key={index}>
+            <button
+              key={index}
+              className='breadcrumb-button'
+              onClick={() => navigate(crumb.path)}
+            >
+              {crumb.label}
+            </button>
+            <span className='breadcrumb-separator'>{">"}</span>
           </React.Fragment>
-          
         ))}
-        
-        <span className="breadcrumb-current">{title}</span>
+
+        <span className='breadcrumb-current'>{title}</span>
       </div>
     </div>
-   
   );
 };
