0876e81631f45917b63fe80d527a110a7bf10a19
Samiran_Das <samiran4209@gmail.com>
2024-11-16 01:05:39 +0530

    push

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -8,7 +8,39 @@ import Labs from './Section/Labs';
 import RecommendedCourses from './Section/RecommendedCourses';
 import SearchResearch from "./Section/SearchResearch";
 import WorkshopPromo from './Section/WorkshopPromo';
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 const LandingPage = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const token = params.get("token");
+
+    if (token) {
+      console.log("Token from URL:", token);
+      const verifyUser = async () => {
+        try {
+          const response = await axios.post(
+            "http://localhost:5001/user/student/verify",
+            {},
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          console.log("Response data:", response.data);
+        } catch (error) {
+          console.error("Error verifying token:", error);
+        }
+      };
+
+      verifyUser();
+    }
+  }, [location]);
+
   return (
     <>
       <ResponsiveAppBar />
