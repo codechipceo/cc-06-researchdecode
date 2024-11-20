ab0ce3c715e389cef67c7a1ec71189b69cd058f6
anish2210 <anishjaiswal1220@gmail.com>
2024-11-20 18:10:41 +0530

    cc-86cx34vd4-Modify_Consultancy_Page

--- src/Pages/Experts/Experts.jsx
+++ src/Pages/Experts/Experts.jsx
@@ -1,26 +1,49 @@
 import { Box, Container, Grid } from "@mui/material";
 import { TeacherCard } from "../../Components/Cards/TeacherCard";
-import { HeaderTwo } from "../../Components/Headers/HeaderTwo";
 import StatusHandler from "../../Components/statusHandler";
 import { useConsultancyCard } from "../../Hooks/useConsultancyCard";
+import { HeaderThree } from "../../Components/Headers/HeaderThree";
+import SearchBar from "../../Components/Searchbar/SearchBar";
+import PaginationComponent from "../../Components/Pagination/PaginationComponent";
+import { useState } from "react";
 
 const Experts = () => {
+  const { searchQuery, setSearchQuery } = useState("");
+
   const {
     consultancyCardData: teachers,
     isLoading,
     isError,
+    handleSearch: fetchTeachers,
   } = useConsultancyCard();
+
+  const handleChange = (value) => {
+    setSearchQuery(value);
+  };
+
+  const handleSearch = () => {
+    fetchTeachers({ search: searchQuery });
+  };
 
   const breadcrumbPath = [{ label: "Home", path: "/" }];
   return (
     <div>
-      <HeaderTwo title='EXPERTS' breadcrumbPath={breadcrumbPath} />
-      <Container maxWidth='lg' sx={{ marginTop: "40px" }}>
+      <HeaderThree title="Experts" breadcrumbPath={breadcrumbPath} />
+
+      <SearchBar
+        value={searchQuery}
+        handleChange={handleChange}
+        handleSearch={handleSearch}
+        className="input"
+        placeholder="Search for experts"
+      />
+
+      <Container maxWidth="lg" sx={{ marginTop: "40px" }}>
         <Box sx={{ flexGrow: 1, margin: "20px" }}>
           <StatusHandler
             isLoading={isLoading}
             isError={isError}
-            errorMessage='Error loading Experts'
+            errorMessage="Error loading Experts"
             isEmpty={teachers.length === 0}
           />
           {!isLoading && !isError && teachers.length > 0 && (
@@ -34,6 +57,13 @@ const Experts = () => {
           )}
         </Box>
       </Container>
+
+      <PaginationComponent
+        total={1}
+        // limit={limit}
+        // activePage={activePage}
+        // setActivePage={setActivePage}
+      />
     </div>
   );
 };
