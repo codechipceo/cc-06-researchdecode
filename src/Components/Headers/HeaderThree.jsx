b7864b9d80eec6cb3cd4d7730874df761ecc66ff
Yousuf <yousufali06296@gmail.com>
2024-11-22 11:42:28 +0530

    fixed headerthree minor issues

--- src/Components/Headers/HeaderThree.jsx
+++ src/Components/Headers/HeaderThree.jsx
@@ -1,38 +1,40 @@
 import React from "react";
 import ResponsiveAppBar from "../Navbar/Navbar";
 import { useNavigate } from "react-router-dom";
-import Typography from "../../assets/scss/components/Typography"; 
-import defaultBgImage  from '../../assets/Images/bgrp.png'
-export const HeaderThree = ({ title, breadcrumbPath ,backgroundImage}) => {
-
+import Typography from "../../assets/scss/components/Typography";
+import defaultBgImage from "../../assets/Images/bgrp.png";
+export const HeaderThree = ({ title, breadcrumbPath, backgroundImage }) => {
   const navigate = useNavigate();
 
   return (
-    <div className='header-three'>
+    <div className="header-three">
       <ResponsiveAppBar />
       <div
-        className='banner'
+        className="banner"
         style={{ backgroundImage: `url(${backgroundImage ?? defaultBgImage})` }}
       >
-        <Typography size='5xl' variant={"semibold"} className='title'>
+        {/* <Typography size='5xl' variant={"semibold"} className='title'>
+          {title}
+        </Typography> */}
+        <Typography size={"5xl"} variant={"semibold"} className={"titlea"}>
           {title}
         </Typography>
       </div>
-      <div className='breadcrumbs' aria-label='breadcrumb'>
+      <div className="breadcrumbs" aria-label="breadcrumb">
         {breadcrumbPath.map((crumb, index) => (
           <React.Fragment key={index}>
             <button
               key={index}
-              className='breadcrumb-button'
+              className="breadcrumb-button"
               onClick={() => navigate(crumb.path)}
             >
               {crumb.label}
             </button>
-            <span className='breadcrumb-separator'>{">"}</span>
+            <span className="breadcrumb-separator">{">"}</span>
           </React.Fragment>
         ))}
 
-        <span className='breadcrumb-current'>{title}</span>
+        <span className="breadcrumb-current">{title}</span>
       </div>
     </div>
   );
