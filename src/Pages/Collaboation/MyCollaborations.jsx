c436703101646e0a06dc8f87395745a41a56d488
Mansab Mir <mansabmirsde@gmail.com>
2024-11-22 18:03:47 +0530

    My Collab form updated

--- src/Pages/Collaboation/MyCollaborations.jsx
+++ src/Pages/Collaboation/MyCollaborations.jsx
@@ -1,14 +1,19 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { HeaderThree } from "../../Components/Headers/HeaderThree";
 import SearchBar from "../../Components/Searchbar/SearchBar";
-import { collabState, getStudentCollaborations } from "../../Features/Slices/collaborationSlice";
+import {
+  collabState,
+  createCollaboration,
+  getStudentCollaborations,
+} from "../../Features/Slices/collaborationSlice";
 import { selectStudentInfo } from "../../Features/Slices/studentSlice";
 import Typography from "../../assets/scss/components/Typography";
 import { CollaborationCard } from "./components/CollaborationCard";
 import CustomButton from "../../Components/CustomButton/CustomButton";
 import CustomModal from "../../Components/Modal/Modal";
 import useModal from "../../Hooks/useModal";
+import { Form, Input } from "rsuite";
 
 const breadcrumbPath = [
   {
@@ -26,15 +31,16 @@ const MyCollaborations = () => {
 
   const [search, setSearch] = useState("");
   const [searchQuery, setSearchQuery] = useState("");
-  const { isOpen, show, hide} = useModal()
+  const { isOpen, show, hide } = useModal();
+
+  const  [title, setTitle] = useState("")
+  const  [description, setDescription] = useState("")
 
   const studentId = useSelector(selectStudentInfo);
   const dispatch = useDispatch();
   const collabStore = useSelector(collabState);
 
-  const {myCollaborations , loading} = collabStore;
-
-
+  const { myCollaborations, loading } = collabStore;
 
   /*
   ----------------------------------------------------------------
@@ -52,10 +58,25 @@ const MyCollaborations = () => {
     }
   };
 
+  /*
+  ----------------------------------------------------------------
+                          HANDLER FUNCTIONS
+  ----------------------------------------------------------------
+  */
+
+
+
+  const  handleSubmit = () => {
+
+    dispatch(createCollaboration({
+      title,description
+    }))
+
+    hide()
+  }
   useEffect(() => {
     dispatch(
       getStudentCollaborations({
-
         studentId: studentId._id,
       })
     );
@@ -63,31 +84,29 @@ const MyCollaborations = () => {
 
   return (
     <>
-
-    <div>
-      <HeaderThree
-        breadcrumbPath={breadcrumbPath}
-        title={"My Collaborations"}
-      />
-      <div className='default__layout_container'>
-        <Typography size={"3xl"} variant={"bold"} className={"text-center"}>
-          Search Your Collaborations
-        </Typography>
-
-        <SearchBar
-          value={search}
-          handleChange={handleInputChange}
-          handleSearch={handleSearch}
-          placeholder='Search Collaboration'
+      <div>
+        <HeaderThree
+          breadcrumbPath={breadcrumbPath}
+          title={"My Collaborations"}
         />
+        <div className='default__layout_container'>
+          <Typography size={"3xl"} variant={"bold"} className={"text-center"}>
+            Search Your Collaborations
+          </Typography>
 
-      <CustomButton variant={'primary'} onClick={show}>
-        + Add new
-      </CustomButton>
-        <div className='flex collaboration__cards_wrapper flex-wrap'>
-          {myCollaborations.length && !loading
-            ? myCollaborations
-                .map((d) => {
+          <SearchBar
+            value={search}
+            handleChange={handleInputChange}
+            handleSearch={handleSearch}
+            placeholder='Search Collaboration'
+          />
+
+          <CustomButton variant={"primary"} onClick={show}>
+            + Add new
+          </CustomButton>
+          <div className='flex collaboration__cards_wrapper flex-wrap'>
+            {myCollaborations.length && !loading
+              ? myCollaborations.map((d) => {
                   const { _id, title, description } = d;
                   return (
                     <CollaborationCard
@@ -100,29 +119,54 @@ const MyCollaborations = () => {
                     />
                   );
                 })
-            : "Looks like you have not created any collaborations yet. Click Add New to create a new collaboration."}
+              : "Looks like you have not created any collaborations yet. Click Add New to create a new collaboration."}
+          </div>
         </div>
-
-
       </div>
-      </div>
-
 
       <CustomModal open={isOpen} handleClose={hide}>
-
         <CustomModal.Header>
-            header
-
+          <h3>Add New Collaboration Request</h3>
         </CustomModal.Header>
         <CustomModal.Body>
-          body
+          <div>
+            <Form fluid>
+              <Form.Group controlId='title'>
+                <Form.ControlLabel>Title</Form.ControlLabel>
+                <div className='flex'>
+                  <Form.Control
+                    name='title'
+                    onChange={setTitle}
+                    size='md'
+                  />
+                  <Form.HelpText tooltip>Required</Form.HelpText>
+                </div>
+              </Form.Group>
+              <Form.Group controlId='textarea-6'>
+                <Form.ControlLabel>Description</Form.ControlLabel>
+                <Input
+                  as='textarea'
+                  onChange={setDescription}
+                  name='description'
+                />
+              </Form.Group>
+            </Form>
+          </div>
         </CustomModal.Body>
 
         <CustomModal.FooterLeft>
-          Footer left
+          <CustomButton
+            onClick={() => {
+              hide();
+            }}
+          >
+            Cancel
+          </CustomButton>
         </CustomModal.FooterLeft>
         <CustomModal.FooterRight>
-          footer right
+          <CustomButton variant='primary' onClick={handleSubmit}>
+            Submit
+          </CustomButton>
         </CustomModal.FooterRight>
       </CustomModal>
     </>
